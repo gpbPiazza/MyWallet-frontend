874d52684293176db18c55115ee93678b17d5c1c
Gabriel Pedro B Piazza <pedro.piazza99@gmail.com>
2021-01-02 12:49:12 -0300

    add new server URL 2

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,6 +1,6 @@
 const API_DEV_URL = "http://localhost:3000/api/";
 
-const API_PROD_URL = "https://piazza-hello-heroku.herokuapp.com/api/";
+const API_PROD_URL = "https://my-wallet-gpbpiazza.herokuapp.com/api/";
 
 const PRODUCTION = true;
 
