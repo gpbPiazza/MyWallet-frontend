9bdac7d2ca9c011b4716b207d75a7b9a93a2b822
Gabriel Pedro B Piazza <pedro.piazza99@gmail.com>
2021-01-02 11:28:57 -0300

    chore add prod server

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,8 +1,10 @@
 const API_DEV_URL = "http://localhost:3000/api/";
 
-// const API_PROD_URL = "";
+const API_PROD_URL = "https://my-wallet-gpbpiazza.herokuapp.com/api/";
 
-const API_URL = API_DEV_URL;
+const PRODUCTION = true;
+
+const API_URL = PRODUCTION ? API_PROD_URL : API_DEV_URL;
 
 export default API_URL;
 
